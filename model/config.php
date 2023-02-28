3085182b275cf168eada018f19e003c0f36ce41c
momotak <126559222+momotak@users.noreply.github.com>
2023-02-28 17:07:41 +0800

    Update config.php
    增加防护机制，修复了多处SQL注入漏洞

--- model/config.php
+++ model/config.php
@@ -6,4 +6,12 @@ define('DB_NAME','db_cro');
 define('DB_PORT','3306');
 define('DB_TYPE','mysql');
 define('DB_CHARSET','utf8');
+//对输入的转义
+if (!function_exists(get_magic_quotes_gpc) || !get_magic_quotes_gpc()) {
+    foreach (array('_COOKIE', '_POST', '_GET') as $v) {
+        foreach ($$v as $kk => $vv) {
+            $kk{0} != '_' && $$v[$kk] = addslashes($vv);
+        }
+    }
+}
 ?>
